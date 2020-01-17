7951ba436d9c43d983e87aa2140b5e30d1286c1d
alva <alva.kellstrom@hotmail.com>
2020-01-17 15:43:11 +0100

    vad heter du i python,mer svar

--- hej.py
+++ hej.py
@@ -2,7 +2,13 @@
 s=raw_input("Vad heter du?")
 if(s=='Emma'):
   print "Hej Emma Kjellstroem!"
+elif(s=='Urban'):
+  print "Vilken fin t-shirt"
+elif(s=='Ulla'):
+    print "Tror du jag bryr mig?"
+elif(s=='Alva'):
+      print "du e fantastisk"
 else:
-    print  "fyfan vilket fult namn"
+    print "fyfan vilket fult namn"
 
 
